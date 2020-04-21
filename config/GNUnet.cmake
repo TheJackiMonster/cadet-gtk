f8e2d38636eab78713ffd1cc0a07b141595a6076
TheJackiMonster <thejackimonster@gmail.com>
2020-04-21 23:24:45 +0200

    Removed option with submodules because it did not work as intended.

--- config/GNUnet.cmake
+++ config/GNUnet.cmake
@@ -2,43 +2,6 @@
 find_library(GNUNET_LIB_CADET gnunetcadet)
 find_library(GNUNET_LIB_UTIL gnunetutil)
 
-if ((NOT GNUNET_LIB_CADET) AND (NOT GNUNET_LIB_UTIL))
-	set(gnunet_submodule_path "${CMAKE_SOURCE_DIR}/lib/gnunet")
-	
-	if (NOT EXISTS ${gnunet_submodule_path})
-		execute_process(
-				COMMAND git submodule init
-				COMMAND git submodule update lib/gnunet
-				WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-		)
-	endif()
-
-	if (NOT EXISTS ${gnunet_submodule_path}/configure)
-		execute_process(
-				COMMAND ./bootstrap
-				WORKING_DIRECTORY ${gnunet_submodule_path}
-		)
-	endif()
-
-	if (NOT EXISTS ${gnunet_submodule_path}/Makefile)
-		execute_process(
-				COMMAND ./configure
-				WORKING_DIRECTORY ${gnunet_submodule_path}
-		)
-	endif()
-
-	execute_process(
-			COMMAND make
-			COMMAND sudo make install
-			WORKING_DIRECTORY ${gnunet_submodule_path}
-	)
-	
-	find_library(GNUNET_LIB_CADET gnunetcadet)
-	find_library(GNUNET_LIB_UTIL gnunetutil)
-	
-	unset(gnunet_submodule_path)
-endif()
-
 if (NOT GNUNET_LIB_CADET)
 	message(FATAL_ERROR "Important library was not found: gnunetcadet")
 elseif(NOT GNUNET_LIB_UTIL)
